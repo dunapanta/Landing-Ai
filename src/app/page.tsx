525755b78dc40f3f47fc3c7eeef62138c9ba4069
Daniel Unapanta <dunapanta@outlook.es>
2024-03-16 01:51:25 -0500

    Refactor button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,8 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello</h1>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden bg-slate-500">
-
-        <Button className="mt-10">Button</Button>
-
-     
+      <div className="pt-[4.75rem] lg:pt-[5.25rem]">
+        <Button>Hola</Button>
       </div>
     </>
   );
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -11,11 +11,8 @@ export const Button = ({ children, className, white, px }: Props) => {
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className}`;
   return (
-    <div className="relative inline-flex  group">
-      <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
-      <button className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 cursor-pointer">
-        {children}
-      </button>
-    </div>
+    <button className={`button-grad button-grad-g`}>
+      <span>{children}</span>
+    </button>
   );
 };
