29fdc6c2b53eae13537b497e1adb04403cb278a4
Daniel Unapanta <dunapanta@outlook.es>
2024-03-20 20:07:11 -0500

    Add navbar

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,20 +1,39 @@
+import Link from "next/link";
+
 interface Props {
   className?: string;
   onClick?: () => void;
   children: React.ReactNode;
   px?: string;
   isDark?: boolean;
+  href?: string;
 }
 
-export const Button = ({ children, className, isDark, px }: Props) => {
+export const Button = ({
+  children,
+  className,
+  href,
+  isDark,
+  px,
+  onClick,
+}: Props) => {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors ${
     px || "px-7"
   } ${isDark ? "text-n-8" : "text-n-1"} ${className}`;
 
   const spanClasses = "relative z-10";
-  return (
-    <button className={`button-grad ${classes}`}>
+
+  const renderButton = () => (
+    <button className={`button-grad ${classes}`} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
     </button>
   );
+
+  const renderLink = () => (
+    <Link href={href!} onClick={onClick} className={`button-grad ${classes}`}>
+      <span className={spanClasses}>{children}</span>
+    </Link>
+  );
+
+  return href ? renderLink() : renderButton();
 };
