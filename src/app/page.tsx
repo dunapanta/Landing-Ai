346a88c103153de06a94a54ce996299001154a3a
Daniel Unapanta <dunapanta@outlook.es>
2024-03-20 23:02:33 -0500

    Add Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button, Header } from "@/components/ui";
+import { Button, Header, Hero } from "@/components/ui";
 
 export default function Home() {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem]">
         <Header />
+        <Hero />
         {/*  <Button className="rounded-md px-12">Hola</Button> */}
       </div>
     </>
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,7 @@
 export * from "./Button";
 export * from "./Header";
+export * from "./Section";
+export * from "./Hero";
+export * from "./Generating";
+export * from "./Notification";
+export * from "./CompanyLogos";
